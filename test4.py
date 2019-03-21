d72738cb6a3c4c7e0945932bcb9c4cd59f573443
wb.gaoxuejian <wb.gaoxuejian@mesg.corp.netease.com>
2019-03-21 15:44:22 +0800

    访问属性

--- test4.py
+++ test4.py
@@ -20,6 +20,18 @@ emp1 = Employee("Zara", 2000,7)
 "创建 Employee 类的第二个对象"
 emp2 = Employee("Manni", 5000,8)
 
+"""
+getattr(obj, name[, default]) : 访问对象的属性。
+hasattr(obj,name) : 检查是否存在一个属性。
+setattr(obj,name,value) : 设置一个属性。如果属性不存在，会创建一个新属性。
+delattr(obj, name) : 删除属性。
+"""
+hasattr(emp1, 'age')    # 如果存在 'age' 属性返回 True。
+getattr(emp1, 'age')    # 返回 'age' 属性的值
+setattr(emp1, 'age', 8) # 添加属性 'age' 值为 8
+delattr(emp1, 'age')    # 删除属性 'age'
+
+
 emp1.displayEmployee()
 emp2.displayEmployee()
 print ("Total Employee %d" % Employee.empCount)
